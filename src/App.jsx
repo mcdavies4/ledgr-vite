739338154f7ae514ee1051940c19bc755dede9e9
Chukwuemeka Azubuike <azubuikedavies@gmail.com>
2026-04-09 13:49:39 +0100

    Import React alongside useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PayPage from "./PayPage";
 import ContactPage from "./ContactPage";
 import AboutPage from "./AboutPage";
